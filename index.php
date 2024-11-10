ad6be1406edf8aa0a57de73d8252042fe54cfdb1
Andrew5-Dev <topchiy.ap@gmail.com>
2024-11-10 16:37:53 +0200

    trait

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ .  '/app/Controllers/Color.php';
+require_once __DIR__ .  '/app/Controllers/Trait.php';
 
 $color1 = new Color(250, 250, 250);
 $color2 = new Color(0, 255, 0);
@@ -12,5 +13,8 @@ $mixedColor = $color->mix(new Color(100, 100, 100));
 var_dump($mixedColor->getRed());
 var_dump($mixedColor->getGreen());
 var_dump($mixedColor->getBlue());
+
+$test = new Test();
+echo $test->getSum();
 
 
