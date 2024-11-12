bcb59130c1f81bdb56bf9d303101286970bede2a
Andrew5-Dev <topchiy.ap@gmail.com>
2024-11-12 14:38:19 +0200

    Overloading

--- Classes/Trait.php
+++ Classes/Trait.php
@@ -1,27 +1,32 @@
 <?php
 
-trait Trait1 {
+namespace Classes;
+trait Trait1
+{
     public function test(): int
     {
         return 1;
     }
 }
 
-trait Trait2 {
-    public function test() : int
+trait Trait2
+{
+    public function test(): int
     {
         return 2;
     }
 }
 
-trait Trait3 {
+trait Trait3
+{
     public function test(): int
     {
         return 3;
     }
 }
 
-class Test {
+class Test
+{
     use Trait1, Trait2, Trait3 {
         Trait1::test insteadof Trait2, Trait3;
         Trait2::test as test2;
--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 
-require_once __DIR__ .  '/app/Controllers/Color.php';
-require_once __DIR__ .  '/app/Controllers/Trait.php';
+use Classes\Color;
+use Classes\User;
+use Classes\UserException;
 
-$color1 = new Color(250, 250, 250);
+require_once __DIR__ . '/Classes/Color.php';
+require_once __DIR__ . '/Classes/Trait.php';
+require_once __DIR__ . '/Classes/User.php';
+require_once __DIR__ . '/Classes/UserException.php';
+
+/*$color1 = new Color(250, 250, 250);
 $color2 = new Color(0, 255, 0);
 $color3 = new Color(0, 0, 255);
 
@@ -12,9 +18,34 @@ $mixedColor = $color->mix(new Color(100, 100, 100));
 
 var_dump($mixedColor->getRed());
 var_dump($mixedColor->getGreen());
-var_dump($mixedColor->getBlue());
-
-$test = new Test();
-echo $test->getSum();
+var_dump($mixedColor->getBlue());*/
 
 
+try {
+    $user = new User();
+
+    $user->setName("Віктор");
+    $user->setAge(20);
+    $user->setEmail("Email");
+
+    $userData = $user->getAll();
+    print_r($userData);
+
+} catch (UserException $e) {
+    echo "Помилка: " . $e->getMessage() . PHP_EOL;
+}
+
+try {
+    $user = new User();
+
+    $user->setName("Максим");
+    $user->setAge(205);
+
+    $userData = $user->getAll();
+    print_r($userData);
+
+
+
+} catch (UserException $e) {
+    echo "Помилка: " . $e->getMessage() . PHP_EOL;
+}
