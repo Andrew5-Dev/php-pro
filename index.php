555d2de3ed12f3da83c304e2494f7d140209d099
Andrew5-Dev <topchiy.ap@gmail.com>
2024-11-24 16:15:27 +0200

    spl_autoload

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
 
-use Classes\Color;
+spl_autoload_register(function ($namespace) {
+    $namespace = str_replace('\\', DIRECTORY_SEPARATOR, $namespace);
+    $file = __DIR__ . "/$namespace.php";
+
+    echo $file . '<br>';
+
+    if (!file_exists($file)) {
+        throw new Exception("Class $namespace not found");
+    }
+
+    require_once $file;
+});
+
+
+use Classes\Tipes\Color;
 use Classes\User;
 use Classes\UserException;
 
-require_once __DIR__ . '/Classes/Color.php';
-require_once __DIR__ . '/Classes/Trait.php';
-require_once __DIR__ . '/Classes/User.php';
-require_once __DIR__ . '/Classes/UserException.php';
 
-/*$color1 = new Color(250, 250, 250);
+$color1 = new Color(250, 250, 250);
 $color2 = new Color(0, 255, 0);
 $color3 = new Color(0, 0, 255);
 
@@ -18,7 +28,7 @@ $mixedColor = $color->mix(new Color(100, 100, 100));
 
 var_dump($mixedColor->getRed());
 var_dump($mixedColor->getGreen());
-var_dump($mixedColor->getBlue());*/
+var_dump($mixedColor->getBlue());
 
 
 try {
